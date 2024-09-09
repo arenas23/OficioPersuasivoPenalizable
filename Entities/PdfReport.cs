8a71f304def624a20c346e7019e9bf2253a41e41
arenas23 <arenas9808@gmail.com>
2024-09-08 22:26:40 -0500

    feat: add function to extract the information about taxes

--- Entities/PdfReport.cs
+++ Entities/PdfReport.cs
@@ -8,6 +8,27 @@ namespace OficioPersuasivoPenalizable.Entities
 {
     public class PdfReport
     {
+       
+        public Declaracion declaracion;
+        public string total;
+    }
 
+    public class Declaracion
+    {
+        public enum Conceptos
+        {
+            VENTAS,
+            RETENCION,
+            CONSUMO
+        }
+
+        public string? numero;
+        public string? tipo;
+        public string? fecha;
+        public string? concepto;
+        public string? anio;
+        public string? periodo;
+        public string? impuesto;
+        public string? sancion;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using OficioPersuasivoPenalizable.Entities;
+using System.Collections;
 using UglyToad.PdfPig;
 using UglyToad.PdfPig.Content;
 using UglyToad.PdfPig.DocumentLayoutAnalysis.PageSegmenter;
@@ -15,6 +17,13 @@ namespace OficioPersuasivoPenalizable
         {
             string pdfPath = "";
             string text = "";
+            const string FIRST_SENTENCE = "Fecha Concepto Período Impuesto ($)";
+            const string LAST_SENTENCE = "Total ($) :";
+            int firstIndex;
+            int lastIndex;
+            List<string> lines = new();
+            List<Declaracion> taxesLines = new();
+
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 pdfPath = openFileDialog1.FileName;
@@ -27,15 +36,59 @@ namespace OficioPersuasivoPenalizable
                         var words = page.GetWords();
                         //var blocks = DocstrumBoundingBoxes.Instance.GetBlocks(words);
                         var blocks = DefaultPageSegmenter.Instance.GetBlocks(words);
-                        //foreach (var line in blocks[0].TextLines)
-                        //{
-                        //    text += line.Text;
-                        //}
-                        rchTxtBxInfo.Text += blocks[0].TextLines[21].Text;
-                    }
+                        foreach (var line in blocks[0].TextLines)
+                        {
+                            lines.Add(line.Text);
+                        }
                         
+                    }
+
+                    firstIndex = lines.IndexOf(FIRST_SENTENCE);
+                    lastIndex = lines.IndexOf(LAST_SENTENCE);
+
+                    if (firstIndex != -1 && lastIndex != -1)
+                    {
+                        firstIndex++;
+                        lastIndex--;
+
+                        taxesLines = GetDeclaracionList(firstIndex, lastIndex, lines);
+                    }else
+                    {
+                        throw new Exception("No se identifico el inicio de la tabla o el final");
+                    }
+
+
                 }
             }
+        }
+
+        private bool CompareString(string validator, string text)
+        {
+            return validator.ToLower().Equals(text.ToLower());
+        }
+
+        private List<Declaracion> GetDeclaracionList(int startIndex,int lastIndex, List<string> text)
+        {
+            List<Declaracion> declaracionList = new();
+            for(int i = startIndex; i < lastIndex; i++)
+            {
+                string[] declaracionParts = text[i].Split(' ');
+                declaracionList.Add(
+                    new Declaracion
+                    {
+                        numero = declaracionParts[0],
+                        tipo = declaracionParts[1],
+                        fecha = declaracionParts[2],
+                        concepto = declaracionParts[3],
+                        anio = declaracionParts[4],
+                        periodo = declaracionParts[5],
+                        impuesto = declaracionParts[6],
+                        sancion = declaracionParts[7]
+                    }
+                ); 
+            }
+
+            return declaracionList;
         }
     }
 }
