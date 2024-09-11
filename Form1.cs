5b3a0dce3b78a0d3d3210e1556b8c6d7eb0cd572
arenas23 <arenas9808@gmail.com>
2024-09-11 00:54:19 -0500

    refactor: code improved

--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,11 @@ using OficioPersuasivoPenalizable.Entities;
 using System.Collections;
 using UglyToad.PdfPig;
 using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.DocumentLayoutAnalysis;
 using UglyToad.PdfPig.DocumentLayoutAnalysis.PageSegmenter;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.ReadingOrderDetector;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.WordExtractor;
+using UglyToad.PdfPig.Graphics;
 
 namespace OficioPersuasivoPenalizable
 {
@@ -15,51 +19,24 @@ namespace OficioPersuasivoPenalizable
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            string pdfPath = "";
-            string text = "";
-            const string FIRST_SENTENCE = "Fecha Concepto Período Impuesto ($)";
-            const string LAST_SENTENCE = "Total ($) :";
-            int firstIndex;
-            int lastIndex;
-            List<string> lines = new();
-            List<Declaracion> taxesLines = new();
+            string pdfPath;
+           
 
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 pdfPath = openFileDialog1.FileName;
 
-                using (var document = PdfDocument.Open(pdfPath))
-                {
-                    for (var i = 0; i < document.NumberOfPages; i++)
-                    {
-                        var page = document.GetPage(i + 1);
-                        var words = page.GetWords();
-                        //var blocks = DocstrumBoundingBoxes.Instance.GetBlocks(words);
-                        var blocks = DefaultPageSegmenter.Instance.GetBlocks(words);
-                        foreach (var line in blocks[0].TextLines)
-                        {
-                            lines.Add(line.Text);
-                        }
-                        
-                    }
-
-                    firstIndex = lines.IndexOf(FIRST_SENTENCE);
-                    lastIndex = lines.IndexOf(LAST_SENTENCE);
-
-                    if (firstIndex != -1 && lastIndex != -1)
-                    {
-                        firstIndex++;
-                        lastIndex--;
-
-                        taxesLines = GetDeclaracionList(firstIndex, lastIndex, lines);
-                    }else
-                    {
-                        throw new Exception("No se identifico el inicio de la tabla o el final");
-                    }
-
-
-                }
+                
             }
+        }
+
+        private void ExtractInfo(string pdfPath)
+        {
+            List<string> textLines = [];
+            List<Declaracion> taxesLines = [];
+
+            textLines = ReadPdf(pdfPath);
+            GetDeclaracionList(textLines);
         }
 
         private bool CompareString(string validator, string text)
@@ -67,12 +44,42 @@ namespace OficioPersuasivoPenalizable
             return validator.ToLower().Equals(text.ToLower());
         }
 
-        private List<Declaracion> GetDeclaracionList(int startIndex,int lastIndex, List<string> text)
+        private List<string> ReadPdf(string pdfPath)
         {
-            List<Declaracion> declaracionList = new();
-            for(int i = startIndex; i < lastIndex; i++)
+            List<string> lines = [];
+            using (var document = PdfDocument.Open(pdfPath))
             {
-                string[] declaracionParts = text[i].Split(' ');
+                for (var i = 0; i < document.NumberOfPages; i++)
+                {
+                    var page = document.GetPage(i + 1);
+                    var words = page.GetWords();
+                    var blocks = DefaultPageSegmenter.Instance.GetBlocks(words);
+                    foreach (var line in blocks[0].TextLines)
+                    {
+                        lines.Add(line.Text);
+                    }
+
+                }
+                return lines;
+            }
+        }
+        private List<Declaracion> GetDeclaracionList(List<string> lines)
+        {
+            const string FIRST_SENTENCE = "Fecha Concepto Período Impuesto ($)";
+            const string LAST_SENTENCE = "Total ($) :";
+            List<Declaracion> declaracionList = [];
+
+            int firstIndex = lines.IndexOf(FIRST_SENTENCE);
+            int lastIndex = lines.IndexOf(LAST_SENTENCE);
+
+            if (firstIndex == -1 && lastIndex == -1)
+            {
+                throw new Exception("No se identifico el inicio de la tabla o el final");
+            }
+
+            for (int i = firstIndex + 1; i < lastIndex - 1; i++)
+            {
+                string[] declaracionParts = lines[i].Split(' ');
                 declaracionList.Add(
                     new Declaracion
                     {
